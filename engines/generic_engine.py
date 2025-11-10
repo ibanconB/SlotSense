4e2ec4eecf6759dd524388e18216a92c0253a360
IVAN <igstrabajo2002@gmail.com>
2025-11-10 23:01:49 +0100

    Making generic engine modular

--- engines/generic_engine.py
+++ engines/generic_engine.py
@@ -7,9 +7,11 @@ class GenericEngine(BaseEngine):
     Motor genérico: ejecuta un spin básico con Steps genéricos.
     """
 
-    def __init__(self):
-        steps = [
-            BuildBoardStep(),      # Genera el tablero inicial
-            EvaluateWinsStep(),    # Evalúa las ganancias del tablero
-        ]
+    def __init__(self, steps = None):
+        if steps is None:
+            steps = [
+                BuildBoardStep(),      # Genera el tablero inicial
+                EvaluateWinsStep(),    # Evalúa las ganancias del tablero
+            ]
         super().__init__(steps)
+
